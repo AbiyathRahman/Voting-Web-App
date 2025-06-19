b2708b6ea5eef3619f328f9ac3495494bac85c2e
Abiyath Rahman <abiyathrahman@mail.weber.edu>
2025-06-19 11:50:25 -0600

    Implemented test for voting and keeping track of tally

--- test/core_spec.js
+++ test/core_spec.js
@@ -1,7 +1,7 @@
 import {List, Map} from 'immutable';
 import {expect} from 'chai';
 
-import {setEntries} from '../src/core';
+import {setEntries, next} from '../src/core';
 
 describe('application logic', () =>{
     describe('setEntries', () => {
@@ -21,5 +21,70 @@ describe('application logic', () =>{
                 entries: List.of('F1', 'Ballerina')
             })));
         });
+
+        
+    });
+
+    describe('next', () => {
+        it('takes the next two entries under vote', () => {
+            const state = Map({
+                entries: List.of('F1', 'Ballerina', 'SUperman')
+            });
+            const nextState = next(state);
+            expect(nextState).to.equal(Map({
+                vote: Map({
+                    pair: List.of("F1",'Ballerina')
+                }),
+                entries: List.of('Superman')
+            }));
+        });
+    });
+
+    describe('vote', () => {
+        it('creates tally for the voted entry', () => {
+            const state = Map({
+                vote: Map({
+                    pair: List.of('F1', 'Ballerina'),
+                    tally: Map({
+                        'F1': 1
+                    })
+                }),
+                entries: List()  
+            });
+            const nextState = vote(state, 'F1');
+            expect(nextState).to.equal(Map({
+                vote:Map({
+                    pair: List.of('F1','Ballerina'),
+                    tally: Map({
+                        'F1': 1
+                    })
+                }),
+                entries: List()
+            }));
+        });
+
+        it('adds to existing tally', () => {
+            const state = Map({
+                vote: Map({
+                    pair: List.of('F1', 'Ballerina'),
+                    tally: Map({
+                        'F1': 2,
+                        'Ballerina': 2
+                    })
+                }),
+                entries: List()  
+            });
+            const nextState = vote(state, 'F1');
+            expect(nextState).to.equal(Map({
+                vote:Map({
+                    pair: List.of('F1','Ballerina'),
+                    tally: Map({
+                        'F1': 3,
+                        'Ballerina':2
+                    })
+                }),
+                entries: List()
+        }));
+    });
     });
 });
