c027899d4c90fb87996cbc4c2792aed945936c99
Abiyath Rahman <abiyathrahman@mail.weber.edu>
2025-06-21 05:03:12 -0600

    Simplified the voting reducer to not use the whole state but to pick apart and focus on the vote part of the state

--- test/core_spec.js
+++ test/core_spec.js
@@ -97,44 +97,34 @@ describe('application logic', () =>{
     describe('vote', () => {
         it('creates tally for the voted entry', () => {
             const state = Map({
-                vote: Map({
-                    pair: List.of('F1', 'Ballerina')
-                }),
-                entries: List()  
+                pair: List.of('F1', 'Ballerina')
             });
             const nextState = vote(state, 'F1');
             expect(nextState).to.equal(Map({
-                vote:Map({
+                
                     pair: List.of('F1','Ballerina'),
                     tally: Map({
                         'F1': 1
                     })
-                }),
-                entries: List()
             }));
         });
 
         it('adds to existing tally', () => {
             const state = Map({
-                vote: Map({
                     pair: List.of('F1', 'Ballerina'),
                     tally: Map({
                         'F1': 2,
                         'Ballerina': 2
                     })
-                }),
-                entries: List()  
             });
             const nextState = vote(state, 'F1');
             expect(nextState).to.equal(Map({
-                vote:Map({
+               
                     pair: List.of('F1','Ballerina'),
                     tally: Map({
                         'F1': 3,
                         'Ballerina':2
                     })
-                }),
-                entries: List()
         }));
     });
     
