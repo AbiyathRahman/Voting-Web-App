43bdbb611a7039ea8c5f73a0d96eff17815e289f
Abiyath Rahman <abiyathrahman@mail.weber.edu>
2025-06-22 15:11:12 -0600

    Added emit, and on events so it connects to the sever and excecutes actions based on the server

--- src/server.js
+++ src/server.js
@@ -3,5 +3,12 @@ import { Server } from "socket.io";
 export default function startServer(){
     const io = new Server(8090);
     console.log("Server Started");
+    store.subscribe(
+        () => io.emit('state', store.getState().toJs())
+    );
+    io.on('connection', (socket) => {
+        socket.emit('state', store.getState().toJS());
+        socket.on('action', store.dispatch.bind(store));
+    });
     return io;
 }
